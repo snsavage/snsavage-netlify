3eb47ca1a8e29d15d372eb933c3ff4640b15d2c4
Scott Savage <snsavage@gmail.com>
2016-08-03 14:30:55 -0400

    Layout code extracted into partials.

--- config.rb
+++ config.rb
@@ -88,6 +88,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :partials_dir, 'partials'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
