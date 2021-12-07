015cb17a0da9587e3eb36a4bd1b07e844b6a9837
Scott Savage <snsavage@gmail.com>
2021-12-06 21:26:37 -0500

    Add monthly, quarterly, and yearly endpoints

---
+++ functions/monthly.ts
@@ -0,0 +1,39 @@
+import { Handler } from "@netlify/functions";
+import axios from "axios";
+import { DateTime } from "luxon";
+
+const BASE_URL = "https://api.harvestapp.com/v2/reports/time/clients"
+
+const handler: Handler = async function(event, context) {
+    const {
+        HARVEST_ACCESS_TOKEN: access_token,
+        HARVEST_ACCOUNT_ID: account_id,
+        HARVEST_PROJECT_ID: project_id,
+    } = process.env;
+
+    const now = DateTime.now()
+    const lastMonth = now.minus({months: 1});
+
+    const formatDate = (dateTime: DateTime): string => dateTime.toFormat("yyyyLLdd");
+
+    const params = {
+        access_token,
+        account_id,
+        project_id,
+        from: formatDate(lastMonth),
+        to: formatDate(now)
+    };
+
+    const response = await axios.get( BASE_URL, { params });
+
+    const { results } = response?.data;
+    const result = results[0];
+    const totalHours = result.total_hours
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({totalHours})
+    }
+}
+
+export { handler };
